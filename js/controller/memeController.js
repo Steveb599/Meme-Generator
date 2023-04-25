3f33d923659d180ac4f81adad43381d824c4da66
Steveb599 <Steveb599@gmail.com>
2023-04-25 18:16:28 +0300

    added text and rendered first meme onto the canvas

--- js/controller/memeController.js
+++ js/controller/memeController.js
@@ -11,15 +11,14 @@ function renderMeme() {
     gElCanvas = document.querySelector('#my-canvas')
     gCtx = gElCanvas.getContext('2d')
     const elImg = new Image() // Create a new html img element
-    elImg.src = 'images/1.jpg'
+    elImg.src = 'images/5.jpg'
     elImg.onload = () => {
         gCtx.drawImage(elImg, 0, 0, gElCanvas.width, gElCanvas.height)
+        gCtx.font = "30px Poppins"
+        gCtx.fillStyle = "black"
+        gCtx.textAlign = "center"
+        gCtx.fillText("This is a test", gElCanvas.width / 2, gElCanvas.height / 2);
     }
-    gCtx.font = "30px Poppins"
-    gCtx.fillStyle = "black"
-    gCtx.textAlign = "center"
-    gCtx.fillText("This is a test", gElCanvas.width / 2, gElCanvas.height / 2);
 
 }
 
-// function getFont() 
